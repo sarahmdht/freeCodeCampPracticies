77c81722bfc68ca62b1460afb5fa107608fee166
sarahmdht <sarahmdht@gmail.com>
2024-08-21 16:50:29 +0300

    pyramid task

---
+++ 1-PyramidGenerator.js
@@ -0,0 +1,26 @@
+// initialize pyramid
+const character = "#";
+const count = 8;
+const rows = [];
+let inverted = true; // switch to make normal pyramid in case of false and upside down pyramid in case of true
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// controlling direction of the pyramid
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);

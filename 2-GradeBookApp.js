6764feee883a35dc06cd1393bb8cd14b426a0f7c
sarahmdht <sarahmdht@gmail.com>
2024-08-21 16:57:33 +0300

    gradeBook task

---
+++ 2-GradeBookApp.js
@@ -0,0 +1,45 @@
+// calculate average grades
+function getAverage(scores) {
+    let sum = 0;
+  
+    for (const score of scores) {
+      sum += score;
+    }
+  
+    return sum / scores.length;
+  }
+ 
+// Show grades in letters
+  function getGrade(score) {
+    if (score === 100) {
+      return "A++";
+    } else if (score >= 90) {
+      return "A";
+    } else if (score >= 80) {
+      return "B";
+    } else if (score >= 70) {
+      return "C";
+    } else if (score >= 60) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
+  
+  function hasPassingGrade(score) {
+    return getGrade(score) !== "F";
+  }
+  
+  // to send a message to the student with his average, grade, and if he passed or failed
+  function studentMsg(totalScores, studentScore) {
+    let average = getAverage(totalScores);
+    let grade = getGrade(studentScore);
+    
+    if(grade !== "F"){
+      return "Class average: " + average + ". Your grade: " + grade + ". You passed the course."
+    } else {
+      return "Class average: " + average + ". Your grade: " + grade + ". You failed the course."
+    }
+    
+  }
+  console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
